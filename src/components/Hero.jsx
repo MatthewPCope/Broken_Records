25f6ccfbe6787e14bc55ed8ef2adce13300f885d
MatthewPCope <matthew.p.cope@gmail.com>
2024-05-05 14:00:29 -0700

    fixed image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
     return (
         <div className="hero">
             <div>
-                <img src="./src/assets/broken_records.png" alt="Broken Records Logo" />
+                <img src="./assets/Broken_Records_Logo_2.jpg" alt="Broken Records Logo" />
                 {/* <h2>Carmel Valley's Premier 90's Rock Band</h2> */}
             </div>
         </div>
