4bd2fb302bc9796aad9d1f0881073e44aca9e229
MatthewPCope <matthew.p.cope@gmail.com>
2024-05-06 07:36:47 -0700

    round logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
     return (
         <div className="hero">
             <div>
-                <img src="./assets/Broken_Records_Logo_2.jpg" alt="Broken Records Logo" />
+                <img src="Broken_Records_Logo_Round.png" alt="Broken Records Logo" />
                 {/* <h2>Carmel Valley's Premier 90's Rock Band</h2> */}
             </div>
         </div>
