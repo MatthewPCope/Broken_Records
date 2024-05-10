c7be808108f0f43d1e9959994f1d8f9f04e42012
MatthewPCope <matthew.p.cope@gmail.com>
2024-05-10 10:39:23 -0700

    changed everything

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,32 @@
 
 function Hero() {
     return (
-        <div className="hero">
-            <div>
-                <img src="./assets/Broken_Records_Logo_Round.png" alt="Broken Records Logo" />
-                {/* <h2>Carmel Valley's Premier 90's Rock Band</h2> */}
+    <>
+        <div className="header">
+            <div className="container">
+                <div class="navbar">
+                    <div>LOGO</div>
+                    <div>NAVIGATION</div>
+                </div>
             </div>
+            <div class="header-container">
+                <div class="header-container-inner">
+                    <h1>The Broken Records</h1>
+                    <p>A Carmel Valley-based rock group comprised of three beleaguered dads playing grunge and alternative classics from the 90’s and beyond.</p>
+                    <a href="#" class="btn">Upcoming Shows</a>
+                </div>
+            </div>
+            
+            
+            {/* <div>
+                <div class="container">
+                    <h2>Some random heading</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit repellendus adipisci animi porro error, suscipit minus quam mollitia blanditiis voluptatibus facere perspiciatis a architecto pariatur ratione enim molestiae totam culpa?</p>
+                </div>
+            </div> */}
         </div>
+        
+    </>
     )
 }
 
